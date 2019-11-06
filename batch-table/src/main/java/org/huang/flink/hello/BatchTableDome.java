95e22d0d085b6125a6115798c5b19ddf3cd52388
tortoi huang <i-40@163.com>
2019-11-06 16:40:46 +0800

    重构项目结构

---
+++ batch-table/src/main/java/org/huang/flink/hello/BatchTableDome.java
@@ -0,0 +1,47 @@
+package org.huang.flink.hello;
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.java.BatchTableEnvironment;
+import org.apache.flink.table.sinks.CsvTableSink;
+import org.apache.flink.table.sinks.TableSink;
+import org.apache.flink.table.sources.CsvTableSource;
+import org.apache.flink.types.Row;
+
+public class BatchTableDome {
+	static String file1 = "E:\\project\\study\\huang-lab\\python\\webdb\\export_by_ids\\export_by_ids3_1.csv";
+	static TypeInformation stringType = TypeInformation.of(String.class);
+	public static void main(String[] args) throws Exception {
+
+		// set up the execution environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		final CsvTableSource.Builder builder = new CsvTableSource.Builder();
+		builder.path(file1)
+				.field("order_no",stringType)
+				.field("cust_order_no",stringType)
+				.field("status",TypeInformation.of(Byte.class))
+				.field("time1",stringType)
+				.field("time2",stringType)
+				.field("time3",stringType);
+		BatchTableEnvironment environment = BatchTableEnvironment.create(env);
+		environment.registerTableSource("orders",builder.build());
+
+		CsvTableSink sink = new CsvTableSink("E:\\project\\tmp\\output\\flink_sql.csv",",",1, FileSystem.WriteMode.OVERWRITE);
+		final TableSink<Row> configure = sink.configure(new String[] { "status", "count_od" },
+				new TypeInformation[] { TypeInformation.of(Byte.class), TypeInformation.of(Long.class) });
+		environment.registerTableSink("order_group",configure);
+		final long start = System.nanoTime();
+		final Table table = environment.sqlQuery("select status,count(1) from orders group by status order by status");
+
+		System.out.println(environment.explain(table));
+
+		table.insertInto("order_group");
+		//environment.toDataSet(table, Row.class).print();
+		env.execute();
+
+		System.out.println("excute time:" + (System.nanoTime() - start) / 1_000_000);
+	}
+}
---
+++ batch-word-count/src/main/java/org/huang/flink/hello/BatchWordCount.java
@@ -0,0 +1,64 @@
+package org.huang.flink.hello;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class BatchWordCount {
+
+	// *************************************************************************
+	//     PROGRAM
+	// *************************************************************************
+
+	public static void main(String[] args) throws Exception {
+
+		// set up the execution environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		// get input data
+		DataSet<String> text;
+		// get default test text data
+		System.out.println("Executing WordCount example with default input data set.");
+		System.out.println("Use --input to specify file1 input.");
+		text = WordCountData.getDefaultTextLineDataSet(env);
+
+		DataSet<Tuple2<String, Integer>> counts =
+				// split up the lines in pairs (2-tuples) containing: (word,1)
+				text.flatMap(new Tokenizer())
+						// group by the tuple field "0" and sum up tuple field "1"
+						.groupBy(0)
+						.sum(1);
+
+		// emit result
+		System.out.println("Printing result to stdout. Use --output to specify output path.");
+		counts.print();
+
+	}
+
+	// *************************************************************************
+	//     USER FUNCTIONS
+	// *************************************************************************
+
+	/**
+	 * Implements the string tokenizer that splits sentences into words as a user-defined
+	 * FlatMapFunction. The function takes a line (String) and splits it into
+	 * multiple pairs in the form of "(word,1)" ({@code Tuple2<String, Integer>}).
+	 */
+	public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+		@Override
+		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+			// normalize and split the line
+			String[] tokens = value.toLowerCase().split("\\W+");
+
+			// emit the pairs
+			for (String token : tokens) {
+				if (token.length() > 0) {
+					out.collect(new Tuple2<>(token, 1));
+				}
+			}
+		}
+	}
+}
